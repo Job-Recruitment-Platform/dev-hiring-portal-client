c7c05215c05ef3ec6d1d665a48eb47b6fb6e97e1
anhkha30804 <anhkha30804@gmail.com>
2025-09-30 22:29:38 +0700

    feat: add base searchbar

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -7,7 +7,7 @@ type ButtonProps = {
 export default function Button({ children, onClick, className }: ButtonProps) {
    return (
       <button
-         className={`content-baseline rounded-full bg-[#00b14f] px-4 py-2 font-semibold text-white ${className}`}
+         className={`content-baseline rounded-full bg-[#00b14f] px-4 py-2 font-semibold text-white hover:cursor-pointer hover:brightness-105 ${className}`}
          onClick={onClick}
       >
          {children}
--- src/components/search-bar/index.tsx
+++ src/components/search-bar/index.tsx
@@ -1,3 +1,25 @@
+import Button from '@/components/button/Button'
+import { SearchIcon } from 'lucide-react'
+import Location from './Location'
+
 export default function SearchBar() {
-   return <div>SearchBar</div>
+   return (
+      <div className='flex w-[60%] items-center rounded-full bg-white p-3 !pl-4'>
+         <div className='flex-1'>
+            <input
+               type='text'
+               className='w-full focus:outline-none'
+               placeholder='Vị trí tuyển dụng, tên công ty'
+            />
+         </div>
+         <div className='flex items-center gap-x-3'>
+            <div className='border-x border-gray-200 px-2'>
+               <Location />
+            </div>
+            <Button className='flex items-center gap-x-2'>
+               <SearchIcon /> Tìm kiếm
+            </Button>
+         </div>
+      </div>
+   )
 }
