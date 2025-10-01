7ab4238992e377945ee98531e4f86447bcec0653
anhkha30804 <anhkha30804@gmail.com>
2025-10-01 08:09:53 +0700

    feat: enhance Header layout and button styles

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -2,15 +2,15 @@ import Button from '@/components/button/Button'
 
 export default function Header() {
    return (
-      <header className='flex items-center justify-between py-5'>
-         <div>
-            <button>Việc làm 1</button>
-            <button>Cẩm nang nghề nghiệp</button>
+      <header className='flex items-center justify-between px-10 py-5'>
+         <div className='flex items-center gap-x-3 font-semibold'>
+            <button className='hover:text-[#00b14f]'>Việc làm</button>
+            <button className='hover:text-[#00b14f]'>Cẩm nang nghề nghiệp</button>
          </div>
-         <div className='flex items-center'>
-            <Button>Đăng ký</Button>
-            <Button>Đăng nhập</Button>
-            <Button>Đăng tuyển & tìm hồ sơ</Button>
+         <div className='flex items-center gap-x-2'>
+            <Button variant='secondary'>Đăng ký</Button>
+            <Button variant='primary'>Đăng nhập</Button>
+            <Button variant='ghost'>Đăng tuyển & tìm hồ sơ</Button>
          </div>
       </header>
    )
