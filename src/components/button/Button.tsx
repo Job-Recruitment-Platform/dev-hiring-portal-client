251a566177f8dc992e4e5cfea0425715689c23cd
anhkha30804 <anhkha30804@gmail.com>
2025-10-01 08:25:09 +0700

    fix: correct button className and format variantStyles

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -3,7 +3,7 @@ import clsx from 'clsx'
 const variantStyles = {
    primary: 'bg-[#00b14f] text-white hover:brightness-105',
    secondary: 'bg-white border border-[#00b14f] text-[#00b14f]',
-   ghost: 'bg-gray-200/60   hover:bg-gray-300/70'
+   ghost: 'bg-gray-200/60 hover:bg-gray-300/70'
 }
 
 type ButtonProps = {
@@ -17,7 +17,7 @@ export default function Button({ children, onClick, className, variant = 'primar
    return (
       <button
          className={clsx(
-            `content-baseline rounded-full px-4 py-2 text-[15px] font-semibold hover:cursor-pointer`,
+            `flex items-center gap-x-1 rounded-full px-4 py-2 text-[15px] font-semibold hover:cursor-pointer`,
             `${className}`,
             `${variantStyles[variant]}`
          )}
